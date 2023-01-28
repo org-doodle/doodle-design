dfdd7976e13aa110105a30e5abcf3c4ec0d823b7
石头 <tingyan.shen@gmail.com>
2023-01-28 11:17:34 +0800

    Adding PacketMapping spring messaging handler support

--- doodle-design-framework/doodle-design-messaging/src/main/java/org/doodle/design/messaging/PacketMappingMessageHandler.java
+++ doodle-design-framework/doodle-design-messaging/src/main/java/org/doodle/design/messaging/PacketMappingMessageHandler.java
@@ -16,27 +16,83 @@
 package org.doodle.design.messaging;
 
 import java.lang.reflect.AnnotatedElement;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.lang.reflect.Method;
+import java.util.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.core.annotation.AnnotatedElementUtils;
-import org.springframework.core.codec.Encoder;
+import org.springframework.lang.Nullable;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.CompositeMessageCondition;
-import org.springframework.messaging.handler.annotation.reactive.MessageMappingMessageHandler;
+import org.springframework.messaging.handler.DestinationPatternsMessageCondition;
+import org.springframework.messaging.handler.annotation.support.AnnotationExceptionHandlerMethodResolver;
+import org.springframework.messaging.handler.invocation.AbstractExceptionHandlerMethodResolver;
+import org.springframework.messaging.handler.invocation.reactive.AbstractMethodMessageHandler;
+import org.springframework.messaging.handler.invocation.reactive.HandlerMethodArgumentResolver;
+import org.springframework.messaging.handler.invocation.reactive.HandlerMethodReturnValueHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.RouteMatcher;
 
-public class PacketMappingMessageHandler extends MessageMappingMessageHandler {
+public class PacketMappingMessageHandler
+    extends AbstractMethodMessageHandler<CompositeMessageCondition> {
 
-  @Getter @Setter private List<Encoder<?>> encoders = new ArrayList<>();
+  @Getter @Setter @Nullable private RouteMatcher routeMatcher;
+
+  public PacketMappingMessageHandler() {
+    setHandlerPredicate(type -> AnnotatedElementUtils.hasAnnotation(type, Controller.class));
+  }
 
   @Override
+  protected List<? extends HandlerMethodArgumentResolver> initArgumentResolvers() {
+    return null;
+  }
+
+  @Override
+  protected List<? extends HandlerMethodReturnValueHandler> initReturnValueHandlers() {
+    return null;
+  }
+
+  @Override
+  protected CompositeMessageCondition getMappingForMethod(Method method, Class<?> handlerType) {
+    return null;
+  }
+
+  @Nullable
   protected CompositeMessageCondition getCondition(AnnotatedElement element) {
     PacketMapping ann = AnnotatedElementUtils.findMergedAnnotation(element, PacketMapping.class);
-    if (Objects.nonNull(ann) && ann.value() > 0 && ann.cmd() > 0) {
-      return new CompositeMessageCondition();
-    } else {
-      return super.getCondition(element);
+    if (Objects.isNull(ann) || (ann.value() <= 0 && ann.group() <= 0) || ann.cmd() <= 0) {
+      return null;
     }
+
+    return null;
+  }
+
+  @Override
+  protected Set<String> getDirectLookupMappings(CompositeMessageCondition mapping) {
+    Set<String> result = new LinkedHashSet<>();
+    return result;
+  }
+
+  @Override
+  protected RouteMatcher.Route getDestination(Message<?> message) {
+    return (RouteMatcher.Route)
+        message.getHeaders().get(DestinationPatternsMessageCondition.LOOKUP_DESTINATION_HEADER);
+  }
+
+  @Override
+  protected CompositeMessageCondition getMatchingMapping(
+      CompositeMessageCondition mapping, Message<?> message) {
+    return mapping.getMatchingCondition(message);
+  }
+
+  @Override
+  protected Comparator<CompositeMessageCondition> getMappingComparator(Message<?> message) {
+    return (info1, info2) -> info1.compareTo(info2, message);
+  }
+
+  @Override
+  protected AbstractExceptionHandlerMethodResolver createExceptionMethodResolverFor(
+      Class<?> beanType) {
+    return new AnnotationExceptionHandlerMethodResolver(beanType);
   }
 }
